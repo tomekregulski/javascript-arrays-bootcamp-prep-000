ce3398e36bfef41ac5d98d33a914002e668acadf
Thomas Regulski <tomek.regulski@gmail.com>
2020-12-11 13:24:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,6 +18,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return array;
 }
 
-function accessElementInArray(array, index) {
-  return array[index]
-}
+// function accessElementInArray(array, index) {
+//   return array[index]
+// }
